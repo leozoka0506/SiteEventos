59e331d413e10f97c655126d7c2891cf808747e1
Samuel Varela Santos <samu.varelasantos@gmail.com>
2025-06-11 19:57:01 -0300

    Documentação

--- src/models/Event.js
+++ src/models/Event.js
@@ -3,10 +3,11 @@ import mongoose from "mongoose";
 // Define o schema (esquema) para a coleção de eventos
 const eventSchema = new mongoose.Schema(
   {
+    // Campo obrigatório: título do evento
     title: { 
       type: String, 
-      required: [true, "O título do evento é obrigatório"], 
-      minlength: [5, "O título deve ter pelo menos 5 caracteres"]
+      required: [true, "O título do evento é obrigatório"], // Mensagem personalizada se não for fornecido
+      minlength: [5, "O título deve ter pelo menos 5 caracteres"] // Validação de comprimento mínimo
     },
     date: { 
       type: Date, 
